0f6f10eee47c4b0f129bf23063baf038148d530a
Вадим Фатеев <fateevdanon@gmail.com>
2023-02-06 18:41:28 +0100

    04. Metric system in response

--- 04_telegram_bot_weather_forecaster/index.js
+++ 04_telegram_bot_weather_forecaster/index.js
@@ -7,16 +7,16 @@ const lon = "24.0232";
 const apiKey = process.env.API_KEY;
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
 async function getWeather() {
   const fetchedData = await axios.get(URL);
   const { weather, main, wind, name } = fetchedData.data;
   const text = `${name} forecast:
   Weather: ${weather.main},
-    Temperature: ${main.temp},
-    Feels like: ${main.feels_like},
-    Wind speed: ${wind.speed}`;
+    Temperature: ${Math.floor(main.temp)}°C,
+    Feels like: ${Math.floor(main.feels_like)}°C,
+    Wind speed: ${wind.speed} m/s`;
   return text;
 }
 
