920fdbc4a768a37f9794290dcc42ef09f3ca239f
Вадим Фатеев <fateevdanon@gmail.com>
2023-02-09 18:28:23 +0100

    Update index.js

--- 05_telegram_bot_exchange_rates/index.js
+++ 05_telegram_bot_exchange_rates/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 const TelegramBot = require("node-telegram-bot-api");
@@ -5,5 +6,4 @@ const apiKey = process.env.API_KEY;
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const axios = require("axios");
-require("dotenv").config();
 const URL = "https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5";
