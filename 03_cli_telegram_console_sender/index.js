fe64c21d04c502371b1d5aa706fe1e126a2c277b
Вадим Фатеев <fateevdanon@gmail.com>
2023-02-15 16:59:06 +0100

    Update index.js

--- 03_cli_telegram_console_sender/index.js
+++ 03_cli_telegram_console_sender/index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 const { Command } = require("commander");
 const program = new Command();
-require("dotenv").config();
+// require("dotenv").config();
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const chatId = process.env.CHAT_ID;
