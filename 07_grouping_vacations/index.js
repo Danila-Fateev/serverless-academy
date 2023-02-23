e4b7f44ec0aca38e7048fb8ee95bef5db1e30ef4
Вадим Фатеев <fateevdanon@gmail.com>
2023-02-23 19:44:25 +0100

    07.Function that returns new JSON

--- 07_grouping_vacations/index.js
+++ 07_grouping_vacations/index.js
@@ -7,4 +7,34 @@ async function getData() {
 
 async function groupVacations() {
   const data = await getData();
+  const newVac = [];
+
+  data.forEach((el) => {
+    const { user, startDate, endDate } = el;
+    const exists = newVac.find((item) => item.userId === user._id);
+
+    if (!exists) {
+      const newUser = {
+        userId: user._id,
+        userName: user.name,
+        vacations: [
+          {
+            startDate: startDate,
+            endDate: endDate,
+          },
+        ],
+      };
+      newVac.push(newUser);
+    }
+    if (exists) {
+      const foundUser = newVac.find((item) => item.userId === user._id);
+
+      const newVacDate = { startDate: startDate, endDate: endDate };
+      foundUser.vacations.push(newVacDate);
+    }
+  });
+  console.log(JSON.stringify(newVac, 0, 2));
+  return JSON.stringify(JSON.stringify(newVac, 0, 2));
 }
+
+groupVacations();
